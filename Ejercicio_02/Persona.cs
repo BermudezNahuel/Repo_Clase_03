434e3aefe774cee72f0d761f854e7f5a590669f9
Nahuel <Nahuel@DESKTOP-3BU0DCU>
2023-04-04 19:16:32 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio_02/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_02
+{
+    internal class Persona
+    {
+        private string nombre;
+        private string fechaDeNacimiento;
+        private int dni;
+    public Persona(string nombre, string fechaDeNacimiento, int dni)
+    {
+        this.nombre = nombre;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.dni = dni;
+    }
+
+    
+
+
+
+
+
+    }
+
+
+}   
+
